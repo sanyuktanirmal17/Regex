6afdaa6a681df68c1311e8e49bd88454c5878c5a
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-19 00:47:31 +0530

    Added: Last Name Pattern

--- src/Service/UserUpdate.java
+++ src/Service/UserUpdate.java
@@ -21,11 +21,12 @@ public class UserUpdate {
 	 * 
 	 * @param args for main of first name
 	 */
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome in User Registration program");
 		UserUpdate user = new UserUpdate();
 		user.firstname();
+		user.lastname();
 	}
 
 	public boolean firstname() {
@@ -36,4 +37,18 @@ public class UserUpdate {
 		return valid;
 
 	}
+
+	/**
+	 * Last name starts with Cap and has minimum 3 characters
+	 * @return check pattern  valid with name 
+	 */
+
+	public boolean lastname() {
+		System.out.println("Enter Lastname:");
+		String name = sc.next();
+		boolean valid = name.matches("^[A-Z][a-z]{3,}");
+		System.out.println("Last Name is:" + name + "\nvalid: " + valid);
+		return valid;
+	}
+
 }
