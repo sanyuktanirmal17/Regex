d449464e45544507e9508b5e3e611011950316d8
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-19 00:40:30 +0530

    Added: First Name Pattern

---
+++ src/Service/UserUpdate.java
@@ -0,0 +1,39 @@
+package Service;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/************************************************************
+ * Purpose : Create a user registration system using regex.
+ * 
+ * @author sanyukta Nirmal
+ * @Version 1.0
+ * @since 18-06-2021
+ *
+ ************************************************************/
+
+public class UserUpdate {
+	static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * check pattern match for name First name starts with Cap and has minimum 3
+	 * characters
+	 * 
+	 * @param args for main of first name
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome in User Registration program");
+		UserUpdate user = new UserUpdate();
+		user.firstname();
+	}
+
+	public boolean firstname() {
+		System.out.println("Enter name:");
+		String name = sc.next();
+		boolean valid = name.matches("^[A-Z][a-z]{2,}");
+		System.out.println("First Name is:" + name + "\nvalid: " + valid);
+		return valid;
+
+	}
+}
